382a4eeac2ece488b2e0a24e525dfa4c98791b2e
Czarlhd <cesar-lahoud@hotmail.com>
2021-02-17 17:39:04 -0500

    copying dimasss's branch

---
+++ Project-Group-04-Backend/src/test/java/ca/mcgill/ecse321/projectgroup04/AutoRepairShopSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.projectgroup04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoRepairShopSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
