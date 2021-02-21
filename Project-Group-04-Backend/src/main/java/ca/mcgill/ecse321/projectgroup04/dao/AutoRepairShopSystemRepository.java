830308567c3e643eaefb28e24d5f1fe81e456dc5
yasminamatta <yasmina.matta@mail.mcgill.ca>
2021-02-21 15:13:52 -0500

    fixed multiplicities in database model

--- Project-Group-04-Backend/src/main/java/ca/mcgill/ecse321/projectgroup04/dao/AutoRepairShopSystemRepository.java
+++ Project-Group-04-Backend/src/main/java/ca/mcgill/ecse321/projectgroup04/dao/AutoRepairShopSystemRepository.java
@@ -1,52 +1,53 @@
-//package ca.mcgill.ecse321.projectgroup04.dao;
-//
-//import java.sql.Date;
-//import java.sql.Time;
-//import java.util.List;
-//
-//import javax.persistence.EntityManager;
-//import javax.persistence.TypedQuery;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Repository;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//@Repository
-//public class AutoRepairShopSystemRepository {
-//
-//    @Autowired
-//    EntityManager entityManager;
-//
-//    @Transactional
-//    public Person createPerson(String name) {
-//        Person p = new Person();
-//        p.setName(name);
-//        entityManager.persist(p);
-//        return p;
-//    }
-//
-//    @Transactional
-//    public Person getPerson(String name) {
-//        Person p = entityManager.find(Person.class, name);
-//        return p;
-//    }
-//
-//    @Transactional
-//    public Event createEvent(String name, Date date, Time startTime, Time endTime) {
-//        Event e = new Event();
-//        e.setName(name);
-//        e.setDate(date);
-//        e.setStartTime(startTime);
-//        e.setEndTime(endTime);
-//        entityManager.persist(e);
-//        return e;
-//    }
-//
-//    @Transactional
-//    public Event getEvent(String name) {
-//        Event e = entityManager.find(Event.class, name);
-//        return e;
-//    }
-//
-//}
-//
+package ca.mcgill.ecse321.projectgroup04.dao;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import ca.mcgill.ecse321.projectgroup04.model.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public class AutoRepairShopSystemRepository {
+
+   @Autowired
+   EntityManager entityManager;
+
+   @Transactional
+   public User createUser(String name) {
+       User p = new Customer();
+       p.setUserID(name);
+       entityManager.persist(p);
+       return p;
+   }
+
+   @Transactional
+   public User getUser(String name) {
+       User p = entityManager.find(User.class, name);
+       return p;
+   }
+
+   @Transactional
+   public Appointment createAppointment(Customer name, Service date, GarageTechnician startTime, TimeSlot endTime) {
+       Appointment e = new Appointment();
+       e.setCustomer(name);
+       e.setServices(date);
+       e.setTechnician(startTime);
+       e.setTimeSlot(endTime);
+       entityManager.persist(e);
+       return e;
+   }
+
+   @Transactional
+   public Appointment getAppointment(String name) {
+       Appointment e = entityManager.find(Appointment.class, name);
+       return e;
+   }
+
+}
+
