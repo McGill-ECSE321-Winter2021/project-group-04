0969f391f4b8c6c11b235ec7aea72917c69a65e6
yasminamatta <yasmina.matta@mail.mcgill.ca>
2021-02-21 20:32:21 -0500

    changes to testpersistence

---
+++ Project-Group-04-Backend/src/main/java/ca/mcgill/ecse321/projectgroup04/dao/AppointmentReminderRepository.java
@@ -0,0 +1,12 @@
+package ca.mcgill.ecse321.projectgroup04.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import ca.mcgill.ecse321.projectgroup04.model.*;
+
+public interface AppointmentReminderRepository extends CrudRepository<AppointmentReminder, String>{
+   List<AppointmentReminder> findByCustomer(Customer customer);
+   AppointmentReminder findByCustomerAndAppointment(Customer customer, Appointment appointment);
+   AppointmentReminder findByReminderID (String reminderID);
+}
