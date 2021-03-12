d08d4daf52619f63c4dbec079cd381a9b30309bf
mdimas2 <mdimassi23@gmail.com>
2021-03-12 16:24:14 +0200

    PostMapped Appointment

--- Project-Group-04-Backend/src/main/java/ca/mcgill/ecse321/projectgroup04/dto/GarageTechnicianDto.java
+++ Project-Group-04-Backend/src/main/java/ca/mcgill/ecse321/projectgroup04/dto/GarageTechnicianDto.java
@@ -2,17 +2,20 @@ package ca.mcgill.ecse321.projectgroup04.dto;
 
 public class GarageTechnicianDto {
 	private String name;
-
+	private Long technicianId;
 	public GarageTechnicianDto() {
 	}
 
-	public GarageTechnicianDto(String name) {
+	public GarageTechnicianDto(String name, Long id) {
 		this.name = name;
-
+		technicianId=id;
 	}
 
 	public String getName() {
 		return name;
+	}
+	public Long getTechnicianId() {
+		return technicianId;
 	}
 
 }
