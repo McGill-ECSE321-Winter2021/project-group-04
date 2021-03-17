cace48f30117734148ad27d4c8b7195373e6fcd6
artus15 <artusjulliard@gmail.com>
2021-03-17 00:54:43 -0400

    added delete / edit customer, delete car and timeSlot, and fixed the post mappings

--- Project-Group-04-Backend/src/main/java/ca/mcgill/ecse321/projectgroup04/dao/TimeSlotRepository.java
+++ Project-Group-04-Backend/src/main/java/ca/mcgill/ecse321/projectgroup04/dao/TimeSlotRepository.java
@@ -1,5 +1,7 @@
 package ca.mcgill.ecse321.projectgroup04.dao;
 
+import java.sql.Date;
+import java.sql.Time;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
@@ -8,4 +10,5 @@ import ca.mcgill.ecse321.projectgroup04.model.*;
 public interface TimeSlotRepository extends CrudRepository<TimeSlot, Long> {
     TimeSlot findTimeSlotByTimeSlotId(Long timeSlotId);
     List<TimeSlot> findTimeSlotByGarageSpot(Integer garageSpot);
+	TimeSlot findTimeSlotByStartDateAndStartTime(Date startDate, Time startTime);
 }
