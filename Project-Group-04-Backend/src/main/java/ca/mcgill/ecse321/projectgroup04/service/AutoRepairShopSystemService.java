26a99a4224575dc6456537ff31e6479b874d0081
mdimas2 <mdimassi23@gmail.com>
2021-03-18 01:01:45 +0200

    Added screenshots for Profile mappings

--- Project-Group-04-Backend/src/main/java/ca/mcgill/ecse321/projectgroup04/service/AutoRepairShopSystemService.java
+++ Project-Group-04-Backend/src/main/java/ca/mcgill/ecse321/projectgroup04/service/AutoRepairShopSystemService.java
@@ -1222,6 +1222,7 @@ public class AutoRepairShopSystemService {
 		profile.setLastName(lastName);
 		profile.setPhoneNumber(phoneNumber);
 		profile.setZipCode(zipCode);
+		profileRepository.save(profile);
 		return profile;
 	}
 
