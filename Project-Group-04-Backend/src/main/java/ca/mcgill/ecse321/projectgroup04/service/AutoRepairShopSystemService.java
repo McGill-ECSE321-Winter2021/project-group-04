20f9688ad93e08635c705b3000684e7ad62a8373
Czarlhd <cesar-lahoud@hotmail.com>
2021-03-17 19:42:30 -0400

    unused import

--- Project-Group-04-Backend/src/main/java/ca/mcgill/ecse321/projectgroup04/service/AutoRepairShopSystemService.java
+++ Project-Group-04-Backend/src/main/java/ca/mcgill/ecse321/projectgroup04/service/AutoRepairShopSystemService.java
@@ -13,7 +13,6 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class AutoRepairShopSystemService {
