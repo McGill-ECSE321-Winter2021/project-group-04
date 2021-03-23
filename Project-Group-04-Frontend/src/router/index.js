35fc07b8d939a96f3a04537ef0a4b1f7ca3852af
mdimas2 <mdimassi23@gmail.com>
2021-03-23 17:40:20 +0200

    front end

--- Project-Group-04-Frontend/src/router/index.js
+++ Project-Group-04-Frontend/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import AutoRepairShop from '@/components/AutoRepairShop'
+
 
 Vue.use(Router)
 
@@ -10,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/app',
+      name: 'AutoRepairShop',
+      component: AutoRepairShop
     }
   ]
 })
