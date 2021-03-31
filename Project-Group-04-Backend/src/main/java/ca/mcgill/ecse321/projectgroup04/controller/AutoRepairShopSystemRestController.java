10be6e8b7ec340b9bd2e17de3bae0f1540b8db01
Czarlhd <cesar-lahoud@hotmail.com>
2021-03-31 12:04:06 -0400

    mini edit

--- Project-Group-04-Backend/src/main/java/ca/mcgill/ecse321/projectgroup04/controller/AutoRepairShopSystemRestController.java
+++ Project-Group-04-Backend/src/main/java/ca/mcgill/ecse321/projectgroup04/controller/AutoRepairShopSystemRestController.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.config.Profiles;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
