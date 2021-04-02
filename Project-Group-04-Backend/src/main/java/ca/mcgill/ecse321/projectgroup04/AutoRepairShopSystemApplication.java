d2e71f9db9ad096b2dae443a867cf452685a6b2e
Czarlhd <cesar-lahoud@hotmail.com>
2021-04-02 14:36:10 -0400

    added pop ups to bookable service ad emergency service

--- Project-Group-04-Backend/src/main/java/ca/mcgill/ecse321/projectgroup04/AutoRepairShopSystemApplication.java
+++ Project-Group-04-Backend/src/main/java/ca/mcgill/ecse321/projectgroup04/AutoRepairShopSystemApplication.java
@@ -3,7 +3,6 @@ package ca.mcgill.ecse321.projectgroup04;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
@@ -13,8 +12,9 @@ public class AutoRepairShopSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AutoRepairShopSystemApplication.class, args);
 	}
+
 	@RequestMapping("/")
-	public String greeting(){
+	public String greeting() {
 		return "Hello world!";
 	}
 
