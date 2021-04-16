68a8f7e865e5cd5ddc391507d0a4dd94a01cc383
Czarlhd <cesar-lahoud@hotmail.com>
2021-04-16 18:15:52 -0400

    commit

---
+++ Project-Group-04-Android/app/build/generated/source/buildConfig/androidTest/debug/ca/mcgill/ecse321/projectgroup04/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ca.mcgill.ecse321.projectgroup04.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "ca.mcgill.ecse321.projectgroup04.test";
+  public static final String BUILD_TYPE = "debug";
+}
